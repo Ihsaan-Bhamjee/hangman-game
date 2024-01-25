ed3ef51dbd769ded63e9d5c2c2280fca90911407
Bhamjee, Ihsaan <ihsaan7864@gmail.com>
2024-01-25 22:12:34 +0200

    complete project

--- README.md
+++ README.md
@@ -32,7 +32,7 @@ Start the development server:
    ```bash
    npm run dev
 
-This will start the development server, and you can access the application at http://localhost:3000.
+This will start the development server, and you can access the application at http://localhost:3000 or http://localhost:5173/.
 Play the Hangman game and enjoy!
 
 
---
+++ src/components/Keyboard.tsx
@@ -0,0 +1,41 @@
+const KEYS = [
+    "a", "b", "c", "d", "e", "f", "g", 
+    "h", "i", "j", "k", "l", "m", "n", 
+    "o", "p", "q", "r", "s", "t", "u", 
+    "v", "w", "x", "y", "z",
+]
+type KeyboardProps = {
+    activeLetters: string[]
+    inactiveLetters: string[]
+    addGuessedLetter: (letter:string) => void
+    disabled?: boolean
+}
+
+export const Keyboard = ({activeLetters, inactiveLetters, addGuessedLetter, disabled=false}:KeyboardProps) => {
+    return (
+        <div style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))", 
+            gap: ".5rem"
+        }}>
+            {KEYS.map(key => {
+                const isActive = activeLetters.includes(key)
+                const isInactive = inactiveLetters.includes(key)
+                return (
+                    <button 
+                        key={key} 
+                        disabled = {isActive || isInactive || disabled}
+                        className= {`
+                                ${"btn "} 
+                                ${isActive ? "active" : ""} 
+                                ${isInactive ? "inactive" : ""}
+                            `}
+                        onClick={() => addGuessedLetter(key)}
+                    > 
+                        {key} 
+                    </button>   
+                )
+            })}
+        </div>
+    )
+}
